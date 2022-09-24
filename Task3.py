a8104763f1533dc7ef23201523dcad05ea9478a1
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-09-24 17:45:21 +0700

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -2,8 +2,7 @@ print("Задание3!")
 
 InputData=[]
 OutputData=[]
-ListOfCosts=[[0]*100]*100
-#count=1
+ListOfCosts=[]
 
 amountofoutputdata=0
 amountofinputdata=int(input("Введите количество строк формата {ряд} {место} {стоимость билета}: "))
@@ -12,23 +11,25 @@ print("Вводите билеты в формате {ряд} {место} {ст
 for i in range(0,amountofinputdata):
     InputData.append(input().split(' '))
 
-for i in range(0,amountofinputdata):
+for i in range(0,len(InputData)):
     if(len(OutputData)==0):
         OutputData.append(InputData[i])
-        #OutputData[0][2] = count
-        #amountofoutputdata=amountofoutputdata+1
     else:
-        count=1
-        for j in range(0,len(OutputData)):
-            if(InputData[i][0] == OutputData[j][0] and InputData[i][1] == OutputData[j][1]):
-                count = count + 1
-                #OutputData[j][2] = count
         for j in range(0, len(OutputData)):
-            if (InputData[i][0] == OutputData[j][0] and InputData[i][1] == OutputData[j][1]):
-                break
-            if(InputData[i][0] != OutputData[j][0] and InputData[i][1] != OutputData[j][1]):
-                OutputData.append(InputData[i])
-                #OutputData[j][2] = count
+            for k in range(0,len(InputData)):
+                count1=0
+                if(InputData[k][0] != OutputData[j][0] or InputData[k][1] != OutputData[j][1]):
+                    for l in range(0,len(OutputData)):
+                        if(InputData[k][0] == OutputData[l][0] and InputData[k][1] == OutputData[l][1]):
+                            count1=1
+                            break
+                    if(count1==1):
+                        count1=0
+                        break
+                    else:
+                        OutputData.append(InputData[k])
+                #if (InputData[i][0] == OutputData[j][0] and InputData[i][1] == OutputData[j][1]):
+                    #break
 
 count=0
 for i in range(0,len(OutputData)):
@@ -36,17 +37,15 @@ for i in range(0,len(OutputData)):
     for j in range(0,len(InputData)):
        if(InputData[j][0] == OutputData[i][0] and InputData[j][1] == OutputData[i][1]):
            count=count+1
-    OutputData[i][2]=count
+    OutputData[i].append(count)
 
-count=0
 for i in range(0,len(OutputData)):
-    count = 0
     for j in range(0,len(InputData)):
         if (InputData[j][0] == OutputData[i][0] and InputData[j][1] == OutputData[i][1]):
-            ListOfCosts[i][count]=InputData[j][2]
-            count = count + 1
+            ListOfCosts.append(InputData[j][2])
+    OutputData[i].append(len(set(ListOfCosts)))
+    ListOfCosts=[]
 
 print("Вывод")
 for i in range(0,len(OutputData)):
-    print(OutputData[i])
-    print(ListOfCosts[i])
+    print(OutputData[i][0], ' ', OutputData[i][1], ' - ', OutputData[i][4])
