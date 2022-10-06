fbee79b3b2ac17b8142db8fca21c0d3456704111
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-10-06 15:27:30 +0700

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,59 +1,57 @@
-def findzero(listofnumbers):
-    count=0
-    for i in range(0,len(listofnumbers)):
-        if listofnumbers[i]==0:
-            count=count+1
-    return count
+def findzero(listofnumbers): #фунция ищет количество 0
+    count=0 #счетчик нулей
+    for i in range(0,len(listofnumbers)): #перебираем все элементы
+        if listofnumbers[i]==0: #если элемент =0
+            count=count+1 #счетчик+1
+    return count #возвращаем счетчик
 
-def minusoneifnozero(listofnumbers):
-    for i in range(0,len(listofnumbers)):
-        listofnumbers[i] = listofnumbers[i] - 1
-    return listofnumbers
+def minusoneifnozero(listofnumbers): #функция отнимает 1 от всех элементов, если нет 0 в последовательности, возвращает новый список
+    for i in range(0,len(listofnumbers)): #перебираем элементы
+        listofnumbers[i] = listofnumbers[i] - 1 #каждый элемент -1
+    return listofnumbers #возвращаем получившийся список
 
-def minusoneifzero(listofnumbers):
-    defcountofoperations=0
-    for i in range(0,len(listofnumbers)):
-        if listofnumbers[i]!=0:
-            if i==0 or listofnumbers[i-1]==0:
+def minusoneifzero(listofnumbers): #функция отнимает 1 от чисел, если есть 0 в последовательности, возвращает новый список
+    defcountofoperations=0 #количество операций в функции
+    for i in range(len(listofnumbers) - 1, -1, -1):
+        if listofnumbers[i]!=0: #если элемент !=0
+            if i==0 or listofnumbers[i-1]==0: #если это первый элемент в списке или если предыдущий был равен нулю
+                listofnumbers[i] = listofnumbers[i] - 1 #элемент-1
+                defcountofoperations = defcountofoperations + 1 #количество операций +1
+            else:
                 listofnumbers[i] = listofnumbers[i] - 1
-                defcountofoperations = defcountofoperations + 1
-    return listofnumbers, defcountofoperations
+    return listofnumbers, defcountofoperations #возвращаем список и количество операций
 
 print("Задание2!")
 
-while True:
+amountofnumbers=4
+numbers=[]*amountofnumbers #список чисел
+amountofzeros=0 #количество 0
+countofoperations=0 #счетчик операций
+countofoperations1=0 #счетчик1
+
+print("Количество вводимых чисел=4, введите 4 числа через пробел")
+
+
+while True: #ввод чисел через пробел
     try:
-        amountofnumbers=int(input("Введите колтчество чисел: "))
+        numbers=list(map(int, input().split()))
         break
     except ValueError:
         print("Вы ввели не число. Попробуйте снова: ")
 
-numbers=[]
-amountofzeros=0
-countofoperations=0
-countofoperations1=0
-
-for i in range(0,amountofnumbers):
-    while True:
-        try:
-            numbers.append(int(input("Введите число: ")))
-            break
-        except ValueError:
-            print("Вы ввели не число. Попробуйте снова: ")
-
-for i in range(0,len(numbers)):
+for i in range(0,len(numbers)): #печать для проверки введеных чисел
     print(numbers[i])
 
-while amountofzeros!=amountofnumbers:
-    amountofzeros=findzero(numbers)
-    if amountofzeros==amountofnumbers:
+while amountofzeros!=amountofnumbers: #пока количесво операций не равно количеству нулей
+    amountofzeros=findzero(numbers) #считаем количество нулей
+    if amountofzeros==amountofnumbers: #если равны, выходим из цикла
         break
-    else:
-        if amountofzeros==0:
-            numbers=minusoneifnozero(numbers)
-            countofoperations=countofoperations+1
-        else:
-            numbers,countofoperations1=minusoneifzero(numbers)
-            countofoperations=countofoperations+countofoperations1
+    else: #иначе
+        if amountofzeros==0: #если количество нулей равно 0
+            numbers=minusoneifnozero(numbers) #отнимаем по 1 и возвращаем новый список
+            countofoperations=countofoperations+1 #увеличиваем количество операций
+        else: #иначе
+            numbers,countofoperations1=minusoneifzero(numbers) #отнимаем по 1 и возвращаем новый список и количество операций произошедших внутри функции
+            countofoperations=countofoperations+countofoperations1 #складываем количество опрераций
 
-print("Количество операций: ", countofoperations)
+print("Количество операций: ", countofoperations) #вывод количества операций
